ee9d4577c098a71b90129c3fe14b203883a269fc
Smertex <kiratsvetkova102@gmail.com>
2023-11-28 09:56:27 +0300

    Добавил удаление точек

--- Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
+++ Task2/ProtoCurveFXApp/src/main/java/com/cgvsu/protocurvefxapp/ProtoCurveController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -27,6 +28,7 @@ public class ProtoCurveController {
         canvas.setOnMouseClicked(event -> {
             switch (event.getButton()) {
                 case PRIMARY -> handlePrimaryClick(canvas.getGraphicsContext2D(), event);
+                case SECONDARY -> handlePrimaryClick(canvas.getGraphicsContext2D(), event);
             }
         });
 
@@ -34,19 +36,42 @@ public class ProtoCurveController {
     }
 
     private void handlePrimaryClick(GraphicsContext graphicsContext, MouseEvent event) {
-        final Point2D clickPoint = new Point2D(event.getX(), event.getY());
 
-        final int POINT_RADIUS = 3;
-        final int POINT_RADIUS_POL = 1;
+        if (event.getButton() == MouseButton.PRIMARY) {
+            final Point2D clickPoint = new Point2D(event.getX(), event.getY());
 
-        graphicsContext.clearRect(0, 0, 800, 600);
+            final int POINT_RADIUS = 3;
+            final int POINT_RADIUS_POL = 1;
 
-        graphicsContext.fillOval(clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS, 2 * POINT_RADIUS, 2 * POINT_RADIUS);
+            graphicsContext.clearRect(0, 0, 800, 600);
 
-        points.add(clickPoint);
+            graphicsContext.fillOval(clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS, 2 * POINT_RADIUS, 2 * POINT_RADIUS);
 
-        if(points.size() > 1) {
-            final Point2D lastPoint = points.get(points.size() - 1);
+            points.add(clickPoint);
+
+            if (points.size() > 1) {
+                final Point2D lastPoint = points.get(points.size() - 1);
+                LagrangePolynomial lagrangePolynomial = new LagrangePolynomial();
+                ArrayList<Point2D> pointsPolynomial = lagrangePolynomial.interpolatePoints(points, points.size());
+
+                for (int i = 0; i < pointsPolynomial.size(); i++) {
+                    graphicsContext.fillOval(pointsPolynomial.get(i).getX() - POINT_RADIUS_POL, pointsPolynomial.get(i).getY() - POINT_RADIUS_POL, POINT_RADIUS_POL * 2, POINT_RADIUS_POL * 2);
+                }
+
+                for (int i = 0; i < points.size() - 1; i++) {
+                    graphicsContext.fillOval(points.get(i).getX() - POINT_RADIUS, points.get(i).getY() - POINT_RADIUS, POINT_RADIUS * 2, POINT_RADIUS * 2);
+                }
+
+            }
+        }
+
+        if (event.getButton() == MouseButton.SECONDARY && points.size() > 1) {
+            points.removeLast();
+            graphicsContext.clearRect(0, 0, 800, 600);
+
+            final int POINT_RADIUS = 3;
+            final int POINT_RADIUS_POL = 1;
+
             LagrangePolynomial lagrangePolynomial = new LagrangePolynomial();
             ArrayList<Point2D> pointsPolynomial = lagrangePolynomial.interpolatePoints(points, points.size());
 
@@ -54,13 +79,9 @@ public class ProtoCurveController {
                 graphicsContext.fillOval(pointsPolynomial.get(i).getX() - POINT_RADIUS_POL, pointsPolynomial.get(i).getY() - POINT_RADIUS_POL, POINT_RADIUS_POL * 2, POINT_RADIUS_POL * 2);
             }
 
-            for(int i = 0; i < points.size() - 1; i++){
+            for (int i = 0; i < points.size(); i++) {
                 graphicsContext.fillOval(points.get(i).getX() - POINT_RADIUS, points.get(i).getY() - POINT_RADIUS, POINT_RADIUS * 2, POINT_RADIUS * 2);
             }
-
         }
-
-
-
     }
 }
